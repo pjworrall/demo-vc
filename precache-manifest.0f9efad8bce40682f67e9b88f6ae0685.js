b58468fcac9d649ee481f99d07c9dfdb6faa6c97
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-25 14:52:17 +0000

    Deploy Vinnl/demo-vc to github.com/Vinnl/demo-vc.git:gh-pages

---
+++ precache-manifest.0f9efad8bce40682f67e9b88f6ae0685.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51535deb24a6b1576a76b6f552416a58",
+    "url": "/demo-vc/index.html"
+  },
+  {
+    "revision": "339ba18ab1195d368cec",
+    "url": "/demo-vc/static/css/main.32154f0e.chunk.css"
+  },
+  {
+    "revision": "b0bc23459f0a6841ec4b",
+    "url": "/demo-vc/static/js/2.938a1c6f.chunk.js"
+  },
+  {
+    "revision": "339ba18ab1195d368cec",
+    "url": "/demo-vc/static/js/main.0ab56d3d.chunk.js"
+  },
+  {
+    "revision": "a6f4b48ee01220f70d50",
+    "url": "/demo-vc/static/js/runtime-main.90a7daa8.js"
+  }
+]);
